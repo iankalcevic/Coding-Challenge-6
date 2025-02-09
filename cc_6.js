0695022e60336f8ae653b0a32c2bd30229a1b6e1
iankalcevic <iankalcevic@usf.edu>
2025-02-09 17:55:02 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -9,3 +9,12 @@ calculateProfit(50, 70, 200); //Expected profit value 4000
 
 console.log("Total Profit: $", calculateProfit(20, 30, 100)); //Log results
 console.log("Total Profit: $", calculateProfit(50, 70, 200)); //Log results 
+
+//Task 2 - Function Expression
+let calculateSalesTax = function(amount, taxRate) {
+    let salesTax =Math.floor(amount * taxRate);
+    return salesTax;
+}; //Calculate sales tax
+
+console.log("Sales Tax: $", calculateSalesTax(100, 0.07)); //100 dollars sales with 7% tax
+console.log("Sales Tax: $", calculateSalesTax(500, 0.1)); //500 dollars sales with 10% tax
