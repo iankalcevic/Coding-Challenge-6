48456f45014373ea84ba1cd5026e58d50a5535e2
iankalcevic <iankalcevic@usf.edu>
2025-02-09 18:01:35 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -18,3 +18,19 @@ let calculateSalesTax = function(amount, taxRate) {
 
 console.log("Sales Tax: $", calculateSalesTax(100, 0.07)); //100 dollars sales with 7% tax
 console.log("Sales Tax: $", calculateSalesTax(500, 0.1)); //500 dollars sales with 10% tax
+
+//Task 3 - Arrow Function
+let calculateBonus = (salary, performanceRating) => {
+    let bonus;
+    if (performanceRating === "Excellent") {
+        bonus = salary * 0.20; //20% Bonus
+    } else if (performanceRating === "Good") {
+        bonus = salary * 0.10; //10% Bonus
+    } else if (performanceRating === "Average") {
+        bonus = salary * 0.05; //5% Bonus
+    }
+    return bonus;
+};
+
+console.log("Bonus 1: $", calculateBonus (5000, "Excellent")) //Logging bonus output
+console.log("Bonus 2: $", calculateBonus (7000, "Good")); //Logging bonus
